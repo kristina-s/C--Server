ae16c6270a30e1e8960cefa1c34cbdd10cf38708
Kristina Spasevska <spasevska.kristina87@gmail.com>
2019-09-19 16:10:17 +0200

    class 03 homework added

---
+++ SedcServer/ServerInterfaces/QueryCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerInterfaces
+{
+    public class QueryCollection
+    {
+        private readonly Dictionary<string, string> data = new Dictionary<string, string>();
+
+        public QueryCollection(Dictionary<string, string> initialData)
+        {
+            data = initialData;
+        }
+
+        public static QueryCollection Empty
+        {
+            get
+            {
+                return new QueryCollection(new Dictionary<string, string>());
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var kvp in data)
+            {
+                sb.AppendLine($"{kvp.Key}: {kvp.Value}");
+            }
+            return sb.ToString();
+        }
+    }
+}
